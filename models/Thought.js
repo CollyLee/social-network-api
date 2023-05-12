6058a5249593c80e841766e97d1f1063c9bc398d
CollyLee <clh5160@gmail.com>
2023-05-12 16:34:53 -0400

    added Thought schema

---
+++ models/Thought.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+// THOUGHT SCHEMA
+const thoughtSchema = new Schema({
+  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+  createdAt: { type: Date, default: Date.now}, /* NEED TO ADD GETTER TO FORMAT DATE */
+  username: { type: String, required: true },
+  reactions: [{ type: Schema.Types.ObjectId, ref: 'Reaction' }]
+}, {
+  toJSON: {
+      virtuals: true,
+  },
+});
+
+// FRIEND COUNT VIRTUAL
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// CREATING THOUGHT MODEL
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
